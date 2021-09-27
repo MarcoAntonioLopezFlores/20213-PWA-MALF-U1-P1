84a9c12912985dfec0a5f6a27dad8e6beaebf4ac
MarcoLF05 <mnmarcolf@gmail.com>
2021-09-27 14:16:56 -0500

    add icons and manifest

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
+let url = window.location.href;
+let swDirect = '/PWA-U1-P1/serviceWorker.js'
+
 if(navigator.serviceWorker){
-    navigator.serviceWorker.register("serviceWorker.js")
+
+    if(url.includes('localhost')){
+        swDirect = '/serviceWorker.js'
+    }
+    navigator.serviceWorker.register(swDirect)
 }else{
     console.log("Ups, cambia de navegador")
 }
